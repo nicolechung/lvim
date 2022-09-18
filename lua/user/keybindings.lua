a4a1101a1666ac7e31deed147968ea1b4b0d9033
Nicole Chung <nicole.chung@gmail.com>
2022-09-18 19:04:52 -0400

    docs: cleanup comments and add telescope file

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -5,7 +5,15 @@ local M = {}
 M.setup = function()
   lvim.leader = "space"
   -- add your own keymapping
+  -- C - control
+  -- S - shift
+  -- A - alt
+  -- MAC command key: don't try to use
+
+  -- save file
   lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+
+  -- make the current tab wider or narrower
   lvim.keys.normal_mode["<C-p>"] = ":vertical resize -2<CR>"
   lvim.keys.normal_mode["<C-o>"] = ":vertical resize +2<CR>"
 end
@@ -16,21 +24,3 @@ return M
 -- unmap a default keymapping
 -- override a default keymapping
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
-
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
--- lua/user/which.lua
+++ lua/user/which.lua
@@ -3,7 +3,9 @@ local M = {}
 M.setup = function()
 
   -- Rip grep find
-  -- "SomeText" ttsx /path/to/directory
+  -- Example:
+  -- "SomeText" thbs /path/to/directory
+  -- finds template/handlebar files containing "SomeText" in a path/to/directory
   lvim.builtin.which_key.mappings["sT"] = {
     "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
     "SuperFind"
