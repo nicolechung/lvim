8b596aa6b8bf12b614af988a75076d90b6cf4d27
Nicole Chung <nicole.chung@crowdstrike.com>
2022-11-01 15:01:09 -0400

    lock plugins to specific version

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -2,20 +2,22 @@ local M = {}
 
 M.setup = function()
   lvim.plugins = {
-    { "folke/tokyonight.nvim" },
+    { "folke/tokyonight.nvim", commit = "81f0db1" },
     {
       "folke/trouble.nvim",
       cmd = "TroubleToggle",
+      commit = "929315e"
     },
-    { "nvim-telescope/telescope-live-grep-args.nvim" },
-    { "hrsh7th/cmp-cmdline" },
-    { 'weilbith/nvim-code-action-menu', cmd = 'CodeActionMenu' },
-    { 'mattn/emmet-vim' },
+    { "nvim-telescope/telescope-live-grep-args.nvim", commit = "32b633b" },
+    { "hrsh7th/cmp-cmdline", commit = "c66c379" },
+    { 'weilbith/nvim-code-action-menu', commit = "ee59940", cmd = 'CodeActionMenu' },
+    { 'mattn/emmet-vim', commit = "def5d57" },
     -- jsdoc
-    { 'kkoomen/vim-doge', run = 'npm i --no-save && npm run build:binary:unix' },
+    { 'kkoomen/vim-doge', commit = "44fcf46", run = ":call doge#install()" },
     -- autotag
     {
       "windwp/nvim-ts-autotag",
+      commit = "fdefe46",
       config = function()
         require("nvim-ts-autotag").setup()
       end,
