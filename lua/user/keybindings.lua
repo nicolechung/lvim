e76b35c15e4577623ebe7221cb7e529fef1de75a
Nicole Chung <nicole.chung@crowdstrike.com>
2022-12-23 15:28:08 -0500

    fix: use shift j and k to move lines up and down again

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -13,17 +13,26 @@ M.setup = function()
   -- insert to normal mode
   -- save file
   lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
- 
+
   -- make the current tab wider or narrower
-  lvim.keys.normal_mode["<C-p>"] = ":vertical resize -2<CR>"
-  lvim.keys.normal_mode["<C-o>"] = ":vertical resize +2<CR>"
+  lvim.keys.normal_mode["<C-p>"] = ":vertical resize -10<CR>"
+  lvim.keys.normal_mode["<C-o>"] = ":vertical resize +10<CR>"
 
   lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
   lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+
   -- map emmet to control+z (easier to reach)
   vim.cmd [[
     inoremap jk <Esc>
     vnoremap jk <Esc>  
+
+    " Moving lines with ^-j/k
+    nnoremap <S-j> :m .+1<CR>==
+    nnoremap <S-k> :m .-2<CR>==
+    " inoremap <C-j> <Esc>:m .+1<CR>==gi
+    " inoremap <C-k> <Esc>:m .-2<CR>==gi
+    vnoremap <S-j> :m '>+1<CR>gv=gv
+    vnoremap <S-k> :m '<-2<CR>gv=gv
     let g:user_emmet_leader_key='<C-z>'
   ]]
 
